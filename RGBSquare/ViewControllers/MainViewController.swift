6024a9d933e19fa99aadc9222760b5ef4ba07a75
Aleksandr Kretov <kretov.1988@gmail.com>
2022-03-18 01:02:25 +0500

    MVC Pattern initial setup

---
+++ RGBSquare/ViewControllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  RGBSquare
+//
+//  Created by Aleksandr Kretov on 18.03.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet var settingButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
